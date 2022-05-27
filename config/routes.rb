53b5c9e379b2727f84ea61a732aff99677f576b0
Estefani Perez <estefani.perez271@gmail.com>
2022-05-27 12:12:39 +0100

    added controller and views for list model, added routes and controller for bookmark model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
   get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  resources :lists, only: %i[index show new create]
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+  end
+
+  resources :bookmarks, only: %i[destroy]
 end
